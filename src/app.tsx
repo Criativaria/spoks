0a9b67d9c73bac5f0d95d8481c2476442bee5a8b
Criativaria <mariaritaboareto@gmail.com>
2024-09-25 21:01:41 -0300

    feat: adapter for storage, in database or asyncStorage

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,11 @@ import { ThemeProvider } from "./context/theme/theme.provider";
 import { StatusBar } from "expo-status-bar";
 import { useFont } from "./hooks/use-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { ClerkProvider } from "@clerk/clerk-expo";
 import { Routes } from "./utils/routes/routes";
 import { tokenCache } from "./api/user/token-cache";
+import { StorageProvider } from "./context/storage/storage-context";
+import { SettingsProvider } from "./context/settings/settings-contex";
 
 const PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
 
@@ -18,10 +20,14 @@ export function App() {
   return (
     <SafeAreaProvider>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} tokenCache={tokenCache}>
-        <ThemeProvider>
-          <StatusBar style="dark" />
-          <Routes />
-        </ThemeProvider>
+        <StorageProvider>
+          <SettingsProvider>
+            <ThemeProvider>
+              <StatusBar style="dark" />
+              <Routes />
+            </ThemeProvider>
+          </SettingsProvider>
+        </StorageProvider>
       </ClerkProvider>
     </SafeAreaProvider>
   );
